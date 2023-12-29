9122be4bf31ee6f8a99865d40e952b92e226e7e3
moonpiderman <lotsofchuck@gmail.com>
2023-12-29 11:20:33 +0900

    Sep: 멀티 모듈화

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,4 +1,5 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+import org.springframework.boot.gradle.tasks.bundling.BootJar
 
 plugins {
     id("org.springframework.boot") version "3.1.7"
@@ -6,6 +7,11 @@ plugins {
     kotlin("jvm") version "1.8.22"
     kotlin("plugin.spring") version "1.8.22"
     kotlin("plugin.jpa") version "1.8.22"
+    kotlin("plugin.allopen") version "1.3.71"
+    kotlin("plugin.noarg") version "1.3.71"
+
+    id("org.jlleitschuh.gradle.ktlint") version "11.3.2"
+    id("org.jlleitschuh.gradle.ktlint-idea") version "11.3.2"
 }
 
 group = "shoyoream.server"
@@ -32,17 +38,81 @@ dependencies {
     testImplementation("org.springframework.kafka:spring-kafka-test")
 }
 
-tasks.withType<KotlinCompile> {
-    kotlinOptions {
-        freeCompilerArgs += "-Xjsr305=strict"
-        jvmTarget = "17"
+allprojects {
+    repositories {
+        mavenCentral()
+        maven("https://jitpack.io")
     }
 }
 
-tasks.withType<Test> {
-    useJUnitPlatform()
+subprojects {
+    apply {
+        plugin("kotlin")
+        plugin("org.springframework.boot")
+        plugin("org.jetbrains.kotlin.jvm")
+        plugin("org.jetbrains.kotlin.plugin.jpa")
+        plugin("org.jetbrains.kotlin.plugin.spring")
+        plugin("kotlin-spring")
+        plugin("io.spring.dependency-management")
+        apply(plugin = "org.jlleitschuh.gradle.ktlint")
+    }
+
+    group = "planningo.server"
+    version = "0.0.1-SNAPSHOT"
+    java.sourceCompatibility = JavaVersion.VERSION_17
+
+    dependencies {
+        implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+        implementation("org.springframework.boot:spring-boot-starter-validation")
+        implementation("org.springframework.boot:spring-boot-starter-web")
+        implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
+        implementation("org.apache.kafka:kafka-streams")
+        implementation("org.jetbrains.kotlin:kotlin-reflect")
+        implementation("org.springframework.kafka:spring-kafka")
+        runtimeOnly("com.mysql:mysql-connector-j")
+        testImplementation("org.springframework.boot:spring-boot-starter-test")
+        testImplementation("org.springframework.kafka:spring-kafka-test")
+    }
+
+    configure<org.jlleitschuh.gradle.ktlint.KtlintExtension> {
+        additionalEditorconfigFile.set(file(".editorconfig"))
+
+        filter {
+            exclude("./.gradle/**")
+            include("**/kotlin/**")
+        }
+    }
+
+    tasks.withType<KotlinCompile> {
+        kotlinOptions {
+            freeCompilerArgs = listOf("-Xjsr305=strict")
+            jvmTarget = "17"
+        }
+    }
+
+    tasks.register("prepareKotlinBuildScriptModel")
+
+    tasks.withType<Test> {
+        useJUnitPlatform()
+    }
 }
 
-tasks.bootBuildImage {
-    builder.set("paketobuildpacks/builder-jammy-base:latest")
+project(":core") {
+    val jar: Jar by tasks
+    val bootJar: BootJar by tasks
+
+    jar.enabled = true
+    bootJar.enabled = false
 }
+
+project(":application") {
+    tasks.bootJar {
+        archiveFileName.set("application.jar")
+    }
+}
+
+val jar: Jar by tasks
+val bootJar: BootJar by tasks
+
+jar.enabled = true
+bootJar.enabled = false
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,1 +1,3 @@
 rootProject.name = "kafka-prac"
+include("core")
+include("application")
